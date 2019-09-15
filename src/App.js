466f3a0b6696ee08c42b7f916a82330f10800236
ElmirMahmudov <elmir9496@gmail.com>
2019-09-15 05:03:41 +0400

    feat(package.json): styled-components and styled-normalize installed and tested

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import { Normalize } from 'styled-normalize';
 import { Button } from 'antd';
 
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+const StyledButton = styled(Button)`
+  font-weight: bold;
+  color: #ddd;
+`;
 function App() {
   return (
-    <div className="App">
-      <Button type="primary" className="myButton">Hello world Button</Button>
-    </div>
+    <React.Fragment>
+      <Normalize />
+      <GlobalStyle />
+      <StyledButton type="primary" className="myButton">Hello world Button</StyledButton>
+    </React.Fragment>
   );
 }
 
